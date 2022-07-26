2293abd37f1a888a46635f7f7aed3f5a14f74533
dynepark9111 <dynepark9111@gmail.com>
2022-07-26 23:38:20 +0900

    feat: darkmode

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,20 @@
 import type { NextPage } from "next";
-import { ReactNode } from "react";
+import { ReactNode, useContext } from "react";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import styles from "../styles/Layout.module.scss";
 import Notification from "./Notification";
+import { darkmodeContext } from "../context/darkmodeContext";
 
 type T = {
   children: ReactNode;
 };
 
 const Layout: NextPage<T> = ({ children }) => {
+  const { isDark } = useContext(darkmodeContext);
+
   return (
-    <div className={styles.Layout}>
+    <div className={styles.Layout} data-theme={isDark ? "dark" : "light"}>
       <div className={styles.top}>
         <Header />
       </div>
