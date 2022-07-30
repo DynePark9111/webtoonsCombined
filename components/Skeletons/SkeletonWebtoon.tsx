4d6906f41fc3d797fafd9c277dfe8b8f0ad791ab
dynepark9111 <dynepark9111@gmail.com>
2022-07-31 00:09:47 +0900

    feat: Comments skeleton

--- components/Skeletons/SkeletonWebtoon.tsx
+++ components/Skeletons/SkeletonWebtoon.tsx
@@ -12,7 +12,7 @@ const SkeletonWebtoon: NextPage = () => {
           {/* Images */}
           <div className={styles.Images}>
             <div className={styles.imagePoster}>
-              <SkeletonElement type={styles.image} />
+              <SkeletonElement type={styles.box} />
             </div>
           </div>
           {/* Icons */}
@@ -25,9 +25,6 @@ const SkeletonWebtoon: NextPage = () => {
             <header className={styles.Header}>
               <ul className={styles.badges}>
                 <li className={styles.rating}>
-                  <SkeletonElement type={styles.text} />
-                </li>
-                <li className={styles.category}>
                   <SkeletonElement type={styles.text} />
                 </li>
               </ul>
@@ -48,15 +45,6 @@ const SkeletonWebtoon: NextPage = () => {
               <div className={styles.links}>
                 <a className={styles.url}>
                   <SkeletonElement type={styles.circle} />
-                  <span>
-                    <SkeletonElement type={styles.empty} />
-                  </span>
-                </a>
-                <a className={styles.firstEpisode}>
-                  <SkeletonElement type={styles.empty} />
-                  <span>
-                    <SkeletonElement type={styles.empty} />
-                  </span>
                 </a>
               </div>
               <div className={styles.section}>
@@ -65,44 +53,13 @@ const SkeletonWebtoon: NextPage = () => {
                     <SkeletonElement type={styles.paragraph} />
                   </div>
                   <div className={styles.Tags}>
-                    <li>
-                      <SkeletonElement type={styles.text} />
-                    </li>
-                    <li>
-                      <SkeletonElement type={styles.text} />
-                    </li>
-                    <li>
-                      <SkeletonElement type={styles.text} />
-                    </li>
+                    <SkeletonElement type={styles.lineLong} />
                   </div>
                 </div>
                 <aside className={styles.aside}>
-                  <ul>
-                    <li>
-                      <h6>
-                        <SkeletonElement type={styles.text} />
-                      </h6>
-                      <span>
-                        <SkeletonElement type={styles.text} />
-                      </span>
-                    </li>
-                    <li>
-                      <h6>
-                        <SkeletonElement type={styles.text} />
-                      </h6>
-                      <span>
-                        <SkeletonElement type={styles.text} />
-                      </span>
-                    </li>
-                    <li>
-                      <h6>
-                        <SkeletonElement type={styles.text} />
-                      </h6>
-                      <span>
-                        <SkeletonElement type={styles.text} />
-                      </span>
-                    </li>
-                  </ul>
+                  <SkeletonElement type={styles.line} />
+                  <SkeletonElement type={styles.line} />
+                  <SkeletonElement type={styles.line} />
                 </aside>
               </div>
             </div>
@@ -119,25 +76,3 @@ const SkeletonWebtoon: NextPage = () => {
 };
 
 export default SkeletonWebtoon;
-
-// <div className={styles.SkeletonWebtoon}>
-//   <div className={styles.top}>
-//     <div className={styles.image}>
-//       <SkeletonElement type={styles.image} />
-//     </div>
-//     <SkeletonElement type={styles.svg} />
-//     <SkeletonElement type={styles.svg} />
-//   </div>
-//   <div className={styles.about}>
-//     <SkeletonElement type={styles.svg} />
-//     <SkeletonElement type={styles.svg} />
-//   </div>
-//   <div className={styles.bottom}>
-//     <SkeletonElement type={styles.svg} />
-//     <SkeletonElement type={styles.svg} />
-//   </div>
-//   aa
-//   <SkeletonElement type="title" />
-//   <SkeletonElement type="text" />
-//   <SkeletonElement type="image" />
-// </div>
