0be905644fbe5d98376af4681475846a4b04e1e3
dynepark9111 <dynepark9111@gmail.com>
2022-08-03 23:16:56 +0900

    commit testing(directory name not changed)

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import styles from "../../styles/Pages/auth/Register.module.scss";
 
 const Register: NextPage = () => {
-  return <div className={styles.Register}>Register</div>;
+  return <div className={styles.Register}>Register.</div>;
 };
 
 export default Register;
